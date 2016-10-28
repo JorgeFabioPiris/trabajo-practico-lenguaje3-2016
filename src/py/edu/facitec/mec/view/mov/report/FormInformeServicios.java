f02d2cf5cd312ec057a308102641fc4b600b05ab
Renacer <jorgefabioasu@hotmail.com>
2016-10-28 11:37:02 -0300

    Trabajo listo para ser presentado para primera evaluacion

--- src/py/edu/facitec/mec/view/mov/report/FormInformeServicios.java
+++ src/py/edu/facitec/mec/view/mov/report/FormInformeServicios.java
@@ -318,15 +318,20 @@ public class FormInformeServicios extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Mov", "Fecha", "Codigo", "Nombre", "Total", "Condicion"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, true, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
